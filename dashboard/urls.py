5d930aa5a000b8ec0039b29fe9545740e70476d5
Samuel Hernan <samuel@everyhost.com>
2023-01-30 20:49:08 +0100

    Solo cambios en base de datos Se programa el servicio de añadir dockers Se añade funcion de parar o iniciar el docker

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,7 +8,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.main,name="main"),
-    path('server', views.dockerForm),
+    path('server', views.dockerForm, name="dockerForm"),
     path('logout', views.userLogout, name="logout"),
+    path('stopDocker/<int:id>', views.stopDocker, name='stopDocker'),
+    path('startDocker/<int:id>', views.startDocker, name='startDocker')
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,11 +7,15 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import DockerForm
+import random,socket,os
 
 @login_required(login_url='login')
 def main(request):
+    user = request.user
+    checkDocker = Docker.objects.filter(user=user).exists()
+    dockerList = Docker.objects.filter(user=user)
     username = request.user.username
-    return render(request,'dashboard.html',{'username':username})
+    return render(request,'dashboard.html',{'username':username,'dockerList':dockerList,'checkDocker':checkDocker})
 
 def userLogout(request):
     logout(request)
@@ -30,9 +34,35 @@ def dockerForm(request):
             user = request.user
             docker = Docker(subdomain=subdomain, repository=repository, software=software, dbadmin=dbadmin, dbpassword=dbpassword, user=user)
             docker.save()
-
+            return redirect('main')
     else:
         form = DockerForm()
     return render(request,'form.html',{'form':form})
 
-        
+
+def stopDocker(request, id):
+    docker = Docker.objects.get(id=id)
+    docker.is_active = False
+    docker.save()
+    return redirect('main')
+
+
+def startDocker(request, id ):
+    docker = Docker.objects.get(id=id)
+    docker.is_active = True
+    docker.port = choosePort()
+    docker.save()
+    return redirect('main')
+
+
+def choosePort():
+    s = socket.socket()
+    while True:
+        port = random.randint(8000, 9000)
+        try:
+            s.bind(("", port))
+            s.close()
+            return port
+        except OSError:
+            # Si el puerto está en uso, volvemos a intentar con otro puerto
+            continue
